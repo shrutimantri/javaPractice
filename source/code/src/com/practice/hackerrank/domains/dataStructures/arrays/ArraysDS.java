efa426491e32be930e08b80b4f441bc922127664
Shruti Mantri <shruti.mantri@PW-C02PQQMXFVH5.local>
2016-08-03 23:48:13 +0530

    Refactoring

---
+++ source/code/src/com/practice/hackerrank/domains/dataStructures/arrays/ArraysDS.java
@@ -0,0 +1,17 @@
+package com.practice.hackerrank.domains.dataStructures.arrays;
+
+import java.util.Scanner;
+
+public class ArraysDS {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[n];
+        for(int i=0; i<n; i++) {
+            array[i] = in.nextInt();
+        }
+        for(int i=n-1; i>=0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
